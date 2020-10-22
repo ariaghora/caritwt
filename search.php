b2bbb2d513354c8958da6c458594f5d64b7d070c
Aria Ghora Prabono <hello@ghora.net>
2020-10-22 23:49:01 +0900

    add initial search api

--- search.php
+++ search.php
@@ -1,3 +1,34 @@
 <?php 
-print("asdsd")
+
+$raw = file_get_contents("http://mobile.twitter.com/search?q=data%20science%20from%3Acollegemenfess");
+
+$dom = new DOMDocument();
+$dom->loadHTML($raw);
+
+$xpath = new DOMXpath($dom);
+$timestamps_xp  = $xpath->query("//td[contains(@class, 'timestamp')]");
+$tweet_texts_xp = $xpath->query("//div[contains(@class, 'tweet-text')]");
+
+$tweet_texts = array();
+$date  = array();
+
+foreach($tweet_texts_xp as $t) {
+    $tweet_texts[] = $t->textContent;
+}
+
+foreach($timestamps_xp as $t) {
+    $date[] = $t->nodeValue;
+}
+
+$out = array();
+for ($i = 0; $i < count($tweet_texts) - 1; $i++) {
+    $o['text'] = trim($tweet_texts[$i]);
+    $o['date'] = trim($date[$i]);
+    $out[] = $o;
+}
+
+header("Content-Type: text/plain");
+header("Access-Control-Allow-Origin: *");
+print(json_encode($out));
+
 ?>
